530247ee8c98057e3613b27488f631c7cea20b39
Griffinseto <Griffinseto@gmail.com>
2020-11-02 00:11:53 -0500

    Add project files.

---
+++ Week2App/Week2App/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Week2App
+{
+    public partial class MainPage : ContentPage
+    {
+        private int clicked = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            mainLabel.Text = "Click Me Has been Clicked";
+
+            this.clicked++;
+
+            Button button = (Button)sender;
+            button.Text = $"Clicked {this.clicked} times";
+        }
+    }
+}
